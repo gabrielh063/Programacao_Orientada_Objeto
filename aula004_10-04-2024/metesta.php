e4308a193af8cfe78aed31c2c60b9c561b22c593
gabrielh063 <gabrielhalves006@gmail.com>
2024-04-10 20:08:06 -0300

    .

---
+++ aula004_10-04-2024/metesta.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Aplicacao
+{
+    static function Sobre(){
+        $fd = fopen('readme.txt', 'r');
+        while($linha = fgets($fd, 200))
+        {
+            echo $linha;
+        }
+    }   
+}
+
+Aplicacao::Sobre();
